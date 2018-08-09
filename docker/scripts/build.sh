6568d588d2cbc7ce250873629fd4e78fd8555cc6
nanielito <danielramirezvi@gmail.com>
2018-08-08 22:40:33 -0400

    #N Docker configuration was added

---
+++ docker/scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+JAR_FILE='com.wgu.setcard.ump-0.1.0-SNAPSHOT.jar'
+
+cd ..
+
+cp ../build/libs/*.jar .
+
+docker build --build-arg JAR_FILE=$JAR_FILE -t setcard/ump:latest .
+
+rm *.jar
